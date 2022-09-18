66e4d03f3187f51d581b4a9a79c22ccaea9cf4de
Olga <olgaharceva946@gmail.com>
2022-09-18 18:40:14 +0300

    Добавили третий пример

---
+++ Homework01/Ex02/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int numberOne = 22;
+int numberTwo = 3;
+int numberThree = 9;
+
+int max = numberOne;
+
+if(numberTwo > max)
+{
+    max = numberTwo;
+}
+if(numberThree > max)
+{
+    max = numberThree;
+}
+
+Console.WriteLine(max);
