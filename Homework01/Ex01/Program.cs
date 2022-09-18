724d18f39c3b5082746631feb2edeea3d5e77932
Olga <olgaharceva946@gmail.com>
2022-09-18 18:18:48 +0300

    Добавлен второй пример

--- Homework01/Ex01/Program.cs
+++ Homework01/Ex01/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int numberOne = 2;
+int numberTwo = 10;
+
+if(numberOne > numberTwo)
+{
+    Console.WriteLine(numberOne);
+}
+else
+{
+    Console.WriteLine(numberTwo);
+}
+
