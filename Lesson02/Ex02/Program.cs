acd56240d40c45b9982fae0abc1ebb23864379e8
Olga <olgaharceva946@gmail.com>
2022-09-20 22:51:56 +0300

    Норм пользовательский UI

--- Lesson02/Ex02/Program.cs
+++ Lesson02/Ex02/Program.cs
@@ -1,13 +1,18 @@
 ﻿// Определить, присутствует ли в заданном массиве, некоторое число
 
+Console.Clear();
 int count = 10;
 int[] array = new int[count];
 int find = new Random().Next(1, 21);
 int index = 0;
+Console.Write("Ищем = ");
+Console.WriteLine(find);
+Console.Write("Массив: ");
 
 while(index < count )
 {
     array[index] = new Random().Next(1, 101);
+    Console.Write(array[index] + " ");
     index++;
 }
 
@@ -16,8 +21,8 @@ while(index < count)
 {
     if(array[index] == find)
     {
-        Console.WriteLine("ДА");
+        Console.WriteLine("\n Такой элемент есть ");
     }
     index++;
 }
-Console.WriteLine("END");
+Console.WriteLine("\n END");
