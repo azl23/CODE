ce668dcc94baa1a03363d54418fa687f072ef8d3
Olga <olgaharceva946@gmail.com>
2022-09-23 21:20:30 +0300

    Добавили первый пример

---
+++ Homework02/Ex01/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int Prompt(string messege)
+{
+    System.Console.Write(messege);           //Ввыводит сообщение
+    string readValue = Console.ReadLine();   // Считывает с консоли строку
+    int result = int.Parse(readValue);       // Преобразует строку в целое число
+    return result;                           // Возвращает результат
+}
+
+int number;
+number = Prompt("Введите трехзначное число ");
+int result = (number % 100) / 10;
+Console.WriteLine($"Вторая цифра чила {number} является {result}");
