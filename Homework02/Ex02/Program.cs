8a13c6775bd237d7cdd75f83cf13d77ada0cd35d
Olga <olgaharceva946@gmail.com>
2022-09-23 22:02:16 +0300

    Вторая домашняя работа

---
+++ Homework02/Ex02/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int Prompt(string messege)
+{
+    System.Console.Write(messege);           //Ввыводит сообщение
+    string readValue = Console.ReadLine();   // Считывает с консоли строку
+    int result = int.Parse(readValue);       // Преобразует строку в целое число
+    return result;                           // Возвращает результат
+}
+
+int number;
+number = Prompt("Введите трехзначное число ");
+int result = (number % 10);
+Console.WriteLine($"Третья цифра чила {number} является {result}");
---
+++ Homework02/Ex03/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int Prompt(string messege)
+{
+    System.Console.Write(messege);           //Ввыводит сообщение
+    string readValue = Console.ReadLine();   // Считывает с консоли строку
+    int result = int.Parse(readValue);       // Преобразует строку в целое число
+    return result;                           // Возвращает результат
+}
+int dayNumber = Prompt("Введите число от 1 до 7 и программа покажет является ли этот день выходным ");
+
+if(dayNumber == 1)
+{
+    Console.WriteLine("Нет");
+}
+
+if (dayNumber == 2)
+{
+    Console.WriteLine("Нет");
+}
+if (dayNumber == 3)
+{
+    Console.WriteLine("Нет");
+}
+if (dayNumber == 4)
+{
+    Console.WriteLine("Нет");
+}
+if (dayNumber == 5)
+{
+    Console.WriteLine("Нет");
+}
+if (dayNumber == 6)
+{
+    Console.WriteLine("Да");
+}
+if (dayNumber == 7)
+{
+    Console.WriteLine("Да");
+}
