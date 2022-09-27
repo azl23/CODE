36e32d1bf64cb87c1aaf89ed4aa940efe124779a
Olga <olgaharceva946@gmail.com>
2022-09-27 21:33:26 +0300

    Решение третьей задачи

---
+++ Homework03/Ex03/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите метод, который принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = int.Parse(value);
+    return result;
+}
+
+void CubNumber(int number)
+{
+    for(int i = 1; i <= number; i++)
+    {
+        Console.WriteLine(i * i * i);;
+    }
+}
+
+int message = Prompt("Введите число ");
+Console.WriteLine($"Таблица кубов чисел от 1 {message} ");
+CubNumber(message); 
