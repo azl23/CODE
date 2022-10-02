063b3f8418d843c03ed2853d91e787a04d0c60df
Olga <olgaharceva946@gmail.com>
2022-10-02 18:44:19 +0300

    Реализовали основной алгоритм

--- Lesson02/Ex02/Program.cs
+++ Lesson02/Ex02/Program.cs
@@ -5,33 +5,22 @@ int count = 10;
 int[] array = new int[count];
 int find = new Random().Next(1, 21);
 int index = 0;
-Console.Write("Ищем = ");
-Console.WriteLine(find);
-Console.Write("Массив: ");
 
 while (index < count)
 {
     array[index] = new Random().Next(1, 101);
-    Console.Write(array[index] + " ");
     index++;
 }
 
 index = 0;
-bool flage = true;
 
 while (index < count)
 {
-    if (array[index] =/= find)
+    if (array[index] == find)
     {
-   
+        Console.WriteLine("ДА ");
     }
-    else
-    {
-    
-    }
-    index++;
+    index += 1;
 }
 
-Console.WriteLine("\n Такой элемент есть ");
-Console.WriteLine("\nТакого числа нет");
-Console.WriteLine("\n END");
+Console.WriteLine("END");
