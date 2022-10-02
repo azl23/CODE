3078d617b6bfa568ab8a4b6b38c0e29831d5c6a9
Olga <olgaharceva946@gmail.com>
2022-10-02 19:11:00 +0300

    Актуальное решение третьей задачи

--- Lesson02/Ex02/Program.cs
+++ Lesson02/Ex02/Program.cs
@@ -5,22 +5,41 @@ int count = 10;
 int[] array = new int[count];
 int find = new Random().Next(1, 21);
 int index = 0;
+Console.Write("Ищем = ");
+Console.WriteLine(find);
+Console.Write("Массив: ");
 
 while (index < count)
 {
-    array[index] = new Random().Next(1, 101);
+    array[index] = new Random().Next(1, 22);
+    Console.WriteLine(array[index] + " ");
     index++;
 }
 
 index = 0;
+bool elementFind = false;
 
 while (index < count)
 {
     if (array[index] == find)
     {
-        Console.WriteLine("ДА ");
+        elementFind = true;
+    }
+    else
+    {
+
     }
     index += 1;
 }
+if (elementFind == true)
+{
+    Console.WriteLine("\nТакой элемент есть ");
+}
+else
+{
+    Console.WriteLine("\nТакого элемента нет");
+}
 
-Console.WriteLine("END");
+Console.WriteLine("\nEND");
+
+
