e8e8f134915d2dbad8eb5b3861fbc320fd64fb0c
Olga <olgaharceva946@gmail.com>
2022-10-11 18:48:19 +0300

    Второй пример лекции(определение по числам)

---
+++ Lecture04/Ex02/Program.cs
@@ -0,0 +1,12 @@
+﻿// Определение на числах
+
+int[,] matrix = new int[3, 4];
+
+for (int i = 0; i < matrix.GetLength(0); i++) //matrix.GetLength(0)-где 0 у нас будет 3
+{
+    for (int j = 0; j < matrix.GetLength(1); j++) //matrix.GetLength(1)- где 1 это наша цифра 4
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
