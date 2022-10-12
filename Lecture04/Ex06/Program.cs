6297f8a72961a2d98f0cee7dff4710a7a07abb8d
Olga <olgaharceva946@gmail.com>
2022-10-12 21:23:55 +0300

    Пример рекурсии с числами Фибиначчи

---
+++ Lecture04/Ex06/Program.cs
@@ -0,0 +1,16 @@
+﻿// Рекурсия с числами Фибиначчи
+
+//f(1) = 1
+//f(2) = 1
+//f(n) = f(n-1) + f(n-2)
+
+double Fibonacci(int n)  //метод для Фибиначчи
+{
+    if(n == 1 || n == 2) return 1;  //если n = 1 или n = 2, то возвращаем 1
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}"); //выведет "f(n) = числа Фибиначчи"
+}
