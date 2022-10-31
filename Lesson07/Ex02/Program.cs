425c45547b2d5c72288ee5f17a1563d382dcdb16
Olga <olgaharceva946@gmail.com>
2022-10-31 22:34:21 +0300

    Подсказка с решением задачи с вещественными числами

---
+++ Lesson07/Ex02/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным
+// элементов массива.
+
+double[] GetArr(int n)
+{
+    double[] arr = new double[n];
+    for (int i = 0; i < 5; i++)
+    {
+        arr[i] = new Random().Next(0, 100) / 10.0; // [0, 10.0)
+    }
+    return arr;
+}
+
+System.Console.WriteLine(String.Join(' ', GetArr(5)));
